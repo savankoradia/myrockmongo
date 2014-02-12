33be0a70fa18f0c4a9f15b5d359a68d98b615c08
savan <sjk.frills2008@gmail.com>
2014-02-12 23:37:44 +0530

    readme updated and config.php changed.

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-myrockmongo
-===========
+Introduction
+--------------------------------------
+MyRockMongo is a MongoDB administration tool, written in PHP 5, very easy to install and use.
 
-MongoDb Administrator.
+
+Installation
+--------------------------------------
+1. Install PHP runtime environment if you don't have one yet, such like Apache Httpd, Nginx ...
+2. Install MongoDB PHP driver (http://us.php.net/manual/en/mongo.installation.php)
+3. Download the package from this git repo.
+4. Unzip the files into your disk, under root of your site
+5. Open the config.php with your convenient editor, change host, port, admins and so on to yours
+6. Visit the index.php in your browser, for example: http://localhost/rockmongo/index.php
+7. Login with admin username and password, which is set "admin" and "admin" as default
+8. Play with your MongoDBs!
+
+
+Upgrade from old version
+--------------------------------------
+1.Copy all files excluding config.php to your old version directory
+2.Done!
+
+
+Bugs & Issues
+--------------------------------------
+Please feel free to report any bugs and issues to me, my email is: koradia.savan@gmail.com
+Blog: http://fedoracores.blogspot.com
--- config.php
+++ config.php
@@ -19,7 +19,7 @@ $i = 0;
 * @see more details at http://rockmongo.com/wiki/configuration?lang=en_us
 */
 $MONGO["servers"][$i]["mongo_name"] = "Localhost";//mongo server name
-$MONGO["servers"][$i]["mongo_host"] = "127.0.0.1";//mongo host
+$MONGO["servers"][$i]["mongo_host"] = "localhost";//mongo host
 $MONGO["servers"][$i]["mongo_port"] = "27017";//mongo port
 $MONGO["servers"][$i]["mongo_timeout"] = 0;//mongo connection timeout
 //$MONGO["servers"][$i]["mongo_db"] = "MONGO_DATABASE";//default mongo db to connect, works only if mongo_auth=false
@@ -27,7 +27,7 @@ $MONGO["servers"][$i]["mongo_timeout"] = 0;//mongo connection timeout
 //$MONGO["servers"][$i]["mongo_pass"] = "MONGO_PASSWORD";//mongo authentication password, works only if mongo_auth=false
 $MONGO["servers"][$i]["mongo_auth"] = false;//enable mongo authentication?
 
-$MONGO["servers"][$i]["control_auth"] = true;//enable control users, works only if mongo_auth=false
+$MONGO["servers"][$i]["control_auth"] = false;//enable control users, works only if mongo_auth=false
 $MONGO["servers"][$i]["control_users"]["admin"] = "admin";//one of control users ["USERNAME"]=PASSWORD, works only if mongo_auth=false
 
 $MONGO["servers"][$i]["ui_only_dbs"] = "";//databases to display
